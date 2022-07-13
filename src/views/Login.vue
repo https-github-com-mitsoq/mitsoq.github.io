d281d3116138383c61c964c79a1cc73c119c9260
mitsoq <mitso.qalaba@superbalist.com>
2022-07-13 19:50:38 +0200

    add recaptcha

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="login">
     <h1>Login</h1>
+
     <form @submit.prevent="loginSubmission">
         <fieldset>
             <label
@@ -18,36 +19,74 @@
                 class="form-control"
                 name="full_name"
             >
-            <input type="submit" value="Submit" />
         </fieldset>
-
+        <vue-recaptcha
+            ref="recaptcha"
+            @verify="onCaptchaVerified"
+            @expired="onCaptchaExpired"
+            size="visible"
+            text-align="left"
+            sitekey="6Lcf3-ogAAAAADMbf9wQmLN9MJI0n22Z_0N7LSS3"
+        ></vue-recaptcha>
+        <input :disabled="status==='submitting'" type="submit" value="Submit" />
     </form>
+
   </section>
 </template>
 
 <script>
+    import VueRecaptcha from 'vue-recaptcha'
     import axios from 'axios'
     export default {
         name: 'Login',
+        components: { VueRecaptcha },
         data () {
             return {
-                full_name: ''
+                full_name: '',
+                recaptchaToken: '',
+                recaptchaSuccessful: false,
+                serverError: '',
+                status: ''
             }
         },
         methods: {
             loginSubmission () {
+                this.$refs.recaptcha.execute();
+            },
+            getErrorMessage (err) {
+                let responseBody;
+                responseBody = err.response;
+                if (!responseBody)
+                    responseBody = err;
+                else
+                    responseBody = err.response.data || responseBody;
+                return responseBody.message || JSON.stringify(responseBody);
+            },
+            onCaptchaVerified () {
+                const self = this
+                self.status = "submitting"
+                self.$refs.recaptcha.reset()
                 axios.post('http://localhost:3000/logins', {
-                    name: this.full_name
+                    name: self.full_name,
+                    recaptchaToken: self.recaptchaToken
                 }, {
                     headers: {
                         'content-type': 'application/json'
                     }
                 }
                 ).then(res => {
+                    this.recaptchaSuccessful = res.body.message
                     console.log('AXIOS', res.body)
                 }).catch(err => {
+                    self.serverError = this.getErrorMessage(err);
                     console.log('ERROR', err)
+                }).then(() => {
+                    self.status = ""
                 })
+            },
+            onCaptchaExpired: function () {
+
+                this.$refs.recaptcha.reset();
             }
         }
     }
