786adb3d53746a782913717dace05c65fa24ef53
mitsoq <mitso.qalaba@superbalist.com>
2022-07-13 17:52:32 +0200

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/mitsoq.github.io/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/mitsoq.github.io/tree/gh-pages" : "/",
 }
