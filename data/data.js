7220cddff6a72dfc7697e12f68ab28f76e3d5fcc
mitsoq <mitso.qalaba@superbalist.com>
2022-07-13 17:43:07 +0200

    commit dist file

---
+++ data/data.js
@@ -0,0 +1,32 @@
+
+const data = {
+    "url": "https://staging.ohsuper.com/feature/basics/106095b8fa",
+    "heading_style": "",
+    "alt_tag": "TESTTTING1",
+    "subtext_style": "",
+    "title": "",
+    "button_style": "",
+    "subtitle": "",
+    "heading": "",
+    "url_target_blank": false,
+    "mobile_image": null,
+    "overlay": false,
+    "subtext": "",
+    "images": [
+        {
+        "base_ssl_url": "https://za.staging.a.sprbl.st/{size}filters:quality({quality}):format({extension})/cms/BASIC-REFRESH_VX3mC0I.gif",
+        "base_url": "http://za.staging.a.sprbl.st/{size}filters:quality({quality}):format({extension})/cms/BASIC-REFRESH_VX3mC0I.gif",
+        "mime_type": null,
+        "filename": "basic refresh 3",
+        "force_png": false,
+        "type": "JPG",
+        "default_serve_extension": null,
+        "aspect_ratio": "1.2521739130434784",
+        "width": 575,
+        "id": 106406,
+        "height": 720,
+        "extension": null,
+        "crop_strategy": null
+        }
+    ]
+};
